3be147f1f99decad6c4eee2a7192e1fc2f261d39
DeazStar <deadfromtheborn@gmail.com>
2023-07-29 11:35:42 +0300

    add the rest of CRUD operations

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -25,6 +25,7 @@ const getAllTask = async (req, res) => {
 
     res.status(200).json({
       status: 'success',
+      resultLength: tasks.length,
       taskLength: await Task.countDocuments(),
       data: {
         tasks,
@@ -38,7 +39,64 @@ const getAllTask = async (req, res) => {
   }
 };
 
+const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      taskLength: await Task.countDocuments(),
+      data: {
+        task,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: `resource not found ${err}`,
+    });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body);
+
+    res.status(200).json({
+      status: 'success',
+      taskLength: await Task.countDocuments(),
+      data: {
+        task: updatedTask,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: `bad request ${err}`,
+    });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: `resource not found ${err}`,
+    });
+  }
+};
+
 module.exports = {
   createTask,
   getAllTask,
+  getTaskById,
+  updateTask,
+  deleteTask,
 };
