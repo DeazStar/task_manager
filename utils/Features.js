2ecae967578baf1ffc287dbade360838607c6b71
DeazStar <deadfromtheborn@gmail.com>
2023-07-29 12:35:42 +0300

    add limiting fields and pagination

--- utils/Features.js
+++ utils/Features.js
@@ -24,6 +24,26 @@ class Features {
     }
     return this;
   }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page || 1;
+    const limit = this.queryString.limit || 10;
+
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
 }
 
 module.exports = Features;
