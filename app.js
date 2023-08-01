3b4058f54f809131f0748e8c0ea0bfc4365a1fab
DeazStar <deadfromtheborn@gmail.com>
2023-08-01 23:31:39 +0300

    add logs plus handle invalid requests

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
 const taskRouter = require('./routes/taskRoutes');
+const AppError = require('./errors/AppError');
+const generalErrorHandler = require('./errors/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(morgan('dev'));
+
 app.use('/api/v1/task', taskRouter);
 
+app.all('*', (req, res, next) => {
+  const message = `Can't find ${req.originalUrl}.`;
+  const statusCode = 404;
+  next(new AppError(message, statusCode));
+});
+
+app.use(generalErrorHandler);
 module.exports = app;
