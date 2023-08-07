cff1a38fb8de0239481a4d59ca8035779f17d527
DeazStar <deadfromtheborn@gmail.com>
2023-08-07 19:51:08 +0300

    add routes for restricting access to admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const { getUsers } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
+
+router
+  .route('/')
+  .get(authController.protect, authController.restrictTo('admin'), getUsers);
 
 module.exports = router;
