0bf9b9ad49eeb8de243c3038dfa57a8c10b54104
DeazStar <deadfromtheborn@gmail.com>
2023-08-07 19:47:43 +0300

    add a function to restrict access for certain routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../errors/catchAsync');
@@ -11,10 +12,11 @@ const sign = (userId) =>
 
 const signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
-    userName: req.body.userName,
+    username: req.body.username,
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role,
   });
 
   const token = sign(newUser._id);
@@ -49,7 +51,50 @@ const login = catchAsync(async (req, res, next) => {
   });
 });
 
+const protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else {
+    return next(
+      new AppError('You must login first to access this resources', 401),
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const user = await User.findById(decoded.userId);
+
+  if (!user) {
+    return next(new AppError('the user of this token no longer exist!', 401));
+  }
+
+  if (await user.checkExpiredToken(decoded.iat)) {
+    return next(
+      new AppError('user recently changed password. please login again', 401),
+    );
+  }
+
+  req.user = user;
+  next();
+});
+
+const restrictTo =
+  (...role) =>
+  async (req, res, next) => {
+    if (!role.includes(req.user.role)) {
+      return next("You don't have permission to perform this action", 403);
+    }
+
+    next();
+  };
+
 module.exports = {
   signup,
   login,
+  protect,
+  restrictTo,
 };
