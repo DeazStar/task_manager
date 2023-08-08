1141f07e48de585cdff282d19ded28c1cd65d67f
DeazStar <deadfromtheborn@gmail.com>
2023-08-08 20:31:48 +0300

    register the update profile methods to there corresponding routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-const { getUsers } = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -11,7 +11,49 @@ router.route('/forgetPassword').post(authController.forgetPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
 router
+  .route('/updateCurrentUser')
+  .patch(authController.protect, userController.updateCurrentUser);
+
+router
+  .route('/deleteCurrentUser')
+  .delete(authController.protect, userController.deleteCurrentUser);
+
+router
+  .route('/updateCurrentPassword')
+  .post(authController.protect, userController.updateCurrentPassword);
+
+router
   .route('/')
-  .get(authController.protect, authController.restrictTo('admin'), getUsers);
+  .get(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.getUsers,
+  );
+
+router
+  .route('/')
+  .post(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.createUser,
+  );
+
+router
+  .route('/:id')
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.updateUser,
+  )
+  .get(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.getUserById,
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.deleteUser,
+  );
 
 module.exports = router;
